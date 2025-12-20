fe032ed7528976644839bb81fe17a0fe68aa4df2
technocubeenergie-arch <techno.cube.energie@gmail.com>
2025-12-20 14:09:41 +0100

    Add game session module

---
+++ www/js/game-session.js
@@ -0,0 +1,24 @@
+(function initializeGameSession(global){
+  if (!global) return;
+
+  const createGameSession = (deps = {}) => {
+    const { Game = global.Game, getGameState = () => undefined } = deps;
+
+    const game = typeof Game === 'function' ? new Game() : null;
+    if (game && game.wallet) global.targetX = game.wallet.x + game.wallet.w / 2;
+
+    const tick = (...args) => (typeof game?.step === 'function' ? game.step(...args) : undefined);
+    const draw = (...args) => (typeof game?.render === 'function' ? game.render(...args) : undefined);
+    const getState = () => ({
+      state: game?.state,
+      gameState: typeof getGameState === 'function' ? getGameState() : undefined,
+    });
+
+    return { game, tick, draw, getState };
+  };
+
+  global.SD_GAME_SESSION = global.SD_GAME_SESSION || {};
+  if (typeof global.SD_GAME_SESSION.createGameSession !== 'function') {
+    global.SD_GAME_SESSION.createGameSession = createGameSession;
+  }
+})(typeof window !== 'undefined' ? window : null);
