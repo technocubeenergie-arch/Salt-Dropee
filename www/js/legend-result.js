e64f5e02cd8b3b693fe69f1bb259aa75e48bcac3
technocubeenergie-arch <techno.cube.energie@gmail.com>
2025-12-30 14:59:24 +0100

    Add legend result scaffold module

---
+++ www/js/legend-result.js
@@ -0,0 +1,5 @@
+(function initLegendResult(global) {
+  const SD_LEGEND_RESULT = global.SD_LEGEND_RESULT || {};
+
+  global.SD_LEGEND_RESULT = SD_LEGEND_RESULT;
+})(typeof window !== 'undefined' ? window : globalThis);
